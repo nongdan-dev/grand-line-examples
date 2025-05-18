7786e40a4405797ba27f7ced4be619425f511443
Nam Nguyen <nam@namnm.com>
2025-05-19 03:39:12 +0700

    partial select

--- simple-todo/src/main.rs
+++ simple-todo/src/main.rs
@@ -12,6 +12,35 @@ pub struct Todo {
     pub done: bool,
 }
 
+// TODO: CODEGEN
+#[derive(FromQueryResult)]
+struct TodoPartialQueryResult {
+    id: Option<String>,
+    content: Option<String>,
+    done: Option<bool>,
+    created_at: Option<DateTimeUtc>,
+    updated_at: Option<DateTimeUtc>,
+}
+impl Into<Todo> for TodoPartialQueryResult {
+    fn into(self) -> Todo {
+        let mut v = Todo::default();
+        if let Some(id) = self.id {
+            v.id = id;
+        }
+        if let Some(content) = self.content {
+            v.content = content;
+        }
+        if let Some(done) = self.done {
+            v.done = done;
+        }
+        if let Some(created_at) = self.created_at {
+            v.created_at = created_at;
+        }
+        v.updated_at = self.updated_at;
+        v
+    }
+}
+
 // search Todo with filter, sort, pagination
 #[search(Todo)]
 fn resolver() {
@@ -40,6 +69,121 @@ fn todoSearch2024() {
     });
     let o = todo_order_by_some!([DoneAsc, ContentAsc]);
     (f, o)
+}
+
+#[derive(Default)]
+pub struct TodoSearch2023Query;
+#[async_graphql::Object]
+impl TodoSearch2023Query {
+    async fn todo_search2023(
+        &self,
+        ctx: &async_graphql::Context<'_>,
+        filter: Option<TodoFilter>,
+        order_by: Option<Vec<TodoOrderBy>>,
+        page: Option<Pagination>,
+    ) -> Result<Vec<Todo>, Box<dyn std::error::Error + Send + Sync>> {
+        let tx = ctx.data_unchecked::<DatabaseConnection>().begin().await?;
+        let r = {
+            // CUSTOM LOGIC FROM RESOVLER
+            // declare extra filter and order by
+            let (extra_filter, default_order_by) = {
+                let f = Some(TodoFilter {
+                    content_starts_with: Some("2023".to_string()),
+                    ..Default::default()
+                });
+                let o = Some(vec![(TodoOrderBy::DoneAsc), (TodoOrderBy::ContentAsc)]);
+                (f, o)
+            };
+            // CODEGEN
+            // combine filter and extra
+            let f = {
+                let f1 = filter;
+                let f2 = extra_filter;
+                if let Some(f1) = f1 {
+                    if let Some(f2) = f2 {
+                        Some(TodoFilter {
+                            and: Some(vec![f1, f2]),
+                            ..Default::default()
+                        })
+                    } else {
+                        Some(f1)
+                    }
+                } else if let Some(f2) = f2 {
+                    Some(f2)
+                } else {
+                    None
+                }
+            };
+            // CODEGEN
+            // combine order by and extra
+            let o = {
+                let o1 = order_by;
+                let o2 = default_order_by;
+                if let Some(o1) = o1 {
+                    if o1.len() > 0 {
+                        Some(o1)
+                    } else {
+                        o2
+                    }
+                } else {
+                    o2
+                }
+            };
+            // CODEGEN
+            // todo_db_search
+            let mut q = Entity::find();
+            if let Some(f) = f {
+                q = f.chain(q);
+            }
+            if let Some(os) = o {
+                if os.len() > 0 {
+                    for o in os {
+                        q = o.chain(q);
+                    }
+                } else {
+                    q = q.order_by_desc(Column::Id);
+                }
+            } else {
+                q = q.order_by_desc(Column::Id);
+            }
+            let mut offset = 0;
+            let mut limit = 100;
+            if let Some(p) = page {
+                if let Some(o) = p.offset {
+                    offset = o;
+                }
+                if let Some(l) = p.limit {
+                    limit = if l > 1000 { 1000 } else { l };
+                }
+            }
+            q = q.offset(offset).limit(limit);
+            // CODEGEN
+            // partial select
+            q = q.select_only();
+            let look_ahead = ctx.look_ahead();
+            if look_ahead.field("id").exists() {
+                q = q.column(TodoColumn::Id)
+            }
+            if look_ahead.field("content").exists() {
+                q = q.column(TodoColumn::Content)
+            }
+            if look_ahead.field("done").exists() {
+                q = q.column(TodoColumn::Done)
+            }
+            if look_ahead.field("createdAt").exists() {
+                q = q.column(TodoColumn::CreatedAt)
+            }
+            if look_ahead.field("updatedAt").exists() {
+                q = q.column(TodoColumn::UpdatedAt)
+            }
+            let r = q.into_model::<TodoPartialQueryResult>().all(&tx).await?;
+            Ok(r.into_iter().map(Into::into).collect())
+        };
+        // CODEGEN
+        // commit
+        tx.commit().await?;
+        r
+    }
 }
 
 // get detail of a Todo by id
@@ -118,16 +262,9 @@ fn todoDeleteDone() -> Vec<Todo> {
     arr
 }
 
-use async_graphql::{
-    http::{AltairConfigOptions, AltairSource, AltairWindowOptions},
-    EmptySubscription, MergedObject, Schema,
-};
+use async_graphql::{EmptySubscription, MergedObject, Schema};
 use async_graphql_axum::GraphQL;
-use axum::{
-    response::{Html, IntoResponse},
-    routing::{get, get_service},
-    serve, Router,
-};
+use axum::{routing::get_service, serve, Router};
 use sea_orm::prelude::*;
 use std::error::Error;
 use tokio::net::TcpListener;
@@ -139,6 +276,7 @@ struct Query(
     TodoSearch2024Query,
     TodoDetailQuery,
     TodoCountDoneQuery,
+    TodoSearch2023Query,
 );
 
 #[derive(Default, MergedObject)]
@@ -152,11 +290,15 @@ struct Mutation(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let schema = Schema::build(Query::default(), Mutation::default(), EmptySubscription)
         .data(init_db().await?)
         .finish();
 
-    let app = Router::new().route("/api/altair", get(altair)).route(
+    let app = Router::new().route(
         "/api/graphql",
         get_service(GraphQL::new(schema.clone())).post_service(GraphQL::new(schema)),
     );
@@ -169,20 +311,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     serve(listener, app).await?;
 
     Ok(())
-}
-
-async fn altair() -> impl IntoResponse {
-    Html(
-        AltairSource::build()
-            .options(AltairConfigOptions {
-                window_options: Some(AltairWindowOptions {
-                    endpoint_url: Some("/api/graphql".to_owned()),
-                    ..Default::default()
-                }),
-                ..Default::default()
-            })
-            .finish(),
-    )
 }
 
 async fn init_db() -> Result<DatabaseConnection, Box<dyn Error>> {
